5208bf372afb165ecce4031abb0ce63dd7229aad
ADARSH-HN <adarshnaik7338@gmail.com>
2026-06-18 16:29:44 +0530

    updated the code with fullfuctionalilty

--- script.js
+++ script.js
@@ -1,29 +1,69 @@
-let str1 ="", str2 ="", operator ="", result ="";
+import {add,subtract,multiply,divide,percentage} from "./calculate.js";
 
+
+let str1="",str2="",result="",operator="";
+
+let textdata=document.querySelector("#text");
 const addtostring = (number) => {
-    if(operator == "" && str1 ==""){
+    if(operator == "" && str1 =="" && number!="."){
         str1=number;
     }
     else if (operator == "" && str1 !=""){
+        if(number==="." && str1.includes(".")){
+            return;
+        }
         str1+=number;
     }
-    else if(operator != "" && str2 ==""){
+    else if(operator != "" && str2 =="" && number!="."){
         str2=number;
     }
-    else if(operator != "" && str2 !=""){
+    else if(operator != "" && str2 !="" ){
+        if(number==="." &&str2.includes(".")){
+            return;
+        }
         str2+=number;
     }
 }
 
-const display =(str1, operator, str2) => {
-    let textdata=document.querySelector("#text");
+const calculate =(str1, str2, operator) => {
+    str1=parseFloat(str1);
+    str2=parseFloat(str2);
+    if(operator==="+"){
+        return add(str1,str2);
+    }
+    else if(operator==="-"){
+        return subtract(str1,str2);
+    }
+    else if(operator==="x"){
+        return multiply(str1,str2);
+    }
+    else if(operator==="/"){
+        return divide(str1,str2);
+    }
+    else if(operator==="%"){
+        return percentage(str1,str2);
+    }
+    else return str1;
+}
+
+
+const display =(str1="", operator="", str2="") => {
     textdata.value=str1+" "+operator+" "+str2;
 }
 
-document.querySelector(".ac").addEventListener("click",() =>{
-    let textdata=document.querySelector("#text");
-    console.log(textdata.value);
-});
+const backspace = () => {
+    if(str2!==""){
+        str2=str2.slice(0,-1);
+    }
+    else if(operator!=""){
+        operator="";
+    }
+    else if(str1!==""){
+        str1=str1.slice(0,-1);
+    }
+    display(str1,operator,str2);
+}
+
 
 let numbers=document.querySelectorAll(".number");
 
@@ -38,8 +78,32 @@ let operators=document.querySelectorAll(".operator");
 
 operators.forEach((op) => {
     op.addEventListener("click",() => {
-       operator=op.innerText; 
-        display(str1, operator, str2);
+        if(op.classList.contains("calc")){
+            if(str1===""){
+                return;
+            }
+            operator=op.innerText; 
+            display(str1, operator, str2);
+        }
+        else if(op.classList.contains("ac")){
+                textdata.value="";
+                operator="";
+                str1="";
+                str2="";
+        }
+        else if(op.classList.contains("equals")){
+            if(str1==="" || (str2==="") || operator===""){
+                return;
+            }
+            result=String(calculate(str1,str2,operator));
+            display(result);
+            str1=result;
+            str2="";
+            operator="";
+        }
+        else if(op.classList.contains("delete")){
+            backspace();
+        }
     })
 });
 
